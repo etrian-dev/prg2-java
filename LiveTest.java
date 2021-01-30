1a8ff322e1b14012f71a9d407123c3cb20fd1671
etrian-dev <nicola.vetrini@gmail.com>
2021-01-30 12:58:36 +0100

    LiveTest now truly interactive

--- LiveTest.java
+++ LiveTest.java
@@ -8,60 +8,58 @@ public class LiveTest {
 
 		Scanner read_in = new Scanner(System.in);
 		// add posts until the user types "fine"
-		String auth, text, ln;
-		Post ps;
+		boolean cont = true;
 		do {
-			System.out.println("Insert author: ");
-			auth = read_in.nextLine();
-			System.out.println("Insert text: ");
-			text = read_in.nextLine();
-			try {
-				// creates an instance with the current UNIX time converted to a date
-				ps = new Post(auth, text, new Date(System.currentTimeMillis()));
-				// tries to add it to the social network
-				net.addPost(ps);
-			} catch (Exception e) {
-				System.out.println("Cannot generate the post: caught exception\n\t" + e);
+			System.out.print("\nAdd post\t[0]\nAdd like\t[1]\nTerminate\t[2]\n>> ");
+			String ln = read_in.nextLine();
+			if(ln.equals("0")) {
+				new_post(net, read_in);
 			}
-			ln = read_in.nextLine();
+			else if(ln.equals("1")) {
+				new_like(net, read_in);
+			}
+			else {
+				System.out.println("Quitting");
+				cont = false;
+			}
 
 			// prints the modified social network
 			System.out.println("Modified Social network:\n" + net.toString());
-		} while (!ln.equals("fine"));
-		// throws away the line containing "fine"
-		read_in.nextLine();
-
-		// posts added: print the social network
-		System.out.println("Posts added.\nSocial network:\n" + net.toString());
-
-		// add likes to posts
-		Integer pid;
-		String dummy;
-		do {
-			System.out.println("Insert pid: ");
-			dummy = read_in.nextLine();
-			pid = Integer.parseInt(dummy);
-			System.out.println("Insert liker: ");
-			dummy = read_in.nextLine();
-			// try to add like to the post
-			try {
-				net.likePost(pid, dummy);
-
-			} catch (Exception e) {
-				System.out.println("Cannot add like the post with pid " + pid + ": caught exception\n\t" + e);
-			}
-			ln = read_in.nextLine();
-
-			// prints the modified social network
-			System.out.println("Modified Social network:\n" + net.toString());
-		} while (!ln.equals("fine"));
+		} while(cont);
 
 		read_in.close();
-
-		// likes added: print the social network
-		System.out.println("Likes added.\nSocial network:\n" + net.toString());
-
+		
+		// print the social network
+		System.out.println("Social network:\n" + net.toString());
 		System.out.println(
 				"Influencers: " + net.influencers().toString() + "\nMentioned users: " + net.getMentionedUsers());
+	}
+	private static void new_post(SocialNetwork net, Scanner s) {
+		System.out.println("Insert author: ");
+		String auth = s.nextLine();
+		System.out.println("Insert text: ");
+		String text = s.nextLine();
+		try {
+			// creates an instance with the current UNIX time converted to a date
+			Post ps = new Post(auth, text, new Date(System.currentTimeMillis()));
+			// tries to add it to the social network
+			net.addPost(ps);
+		} catch (Exception e) {
+			System.out.println("Cannot generate the post: caught exception\n\t" + e);
+		}
+	}
+	private static void new_like(SocialNetwork net, Scanner s) {
+		System.out.println("Insert pid: ");
+		String pidstr = s.nextLine();
+		Integer pid = Integer.parseInt(pidstr);
+		System.out.println("Insert liker: ");
+		String liker = s.nextLine();
+		// try to add like to the post
+		try {
+			net.likePost(pid, liker);
+
+		} catch (Exception e) {
+			System.out.println("Cannot add like the post with pid " + pid + ": caught exception\n\t" + e);
+		}
 	}
 }
